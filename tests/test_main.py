95493a0b12580cca464e4f9a49dff1dc5bef1f97
alehpineda <ale.hpineda@gmail.com>
2023-12-07 18:13:13 -0600

    nueva prueba unitaria

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-from src.main import sumar_numeros_pares
+from src.main import sumar_numeros_impares, sumar_numeros_pares
 
 
 def test_sumar_numeros_pares() -> None:
     assert sumar_numeros_pares([1, 2, 3, 4, 5, 6]) == 12
+
+
+def test_sumar_numeros_impares() -> None:
+    assert sumar_numeros_impares([1, 2, 3, 4, 5, 6]) == 9
