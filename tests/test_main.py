49c578395ba4022a64d15cb8245a6d2b740dcbd7
alehpineda <ale.hpineda@gmail.com>
2023-12-07 18:37:07 -0600

    added more unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,15 @@ from src.main import sumar_numeros_impares, sumar_numeros_pares
 
 def test_sumar_numeros_pares() -> None:
     assert sumar_numeros_pares([1, 2, 3, 4, 5, 6]) == 12
+    # no hay pares
+    assert sumar_numeros_pares([1, 3, 5]) == 0
+    # no hay numeros
+    assert sumar_numeros_pares([]) == 0
 
 
 def test_sumar_numeros_impares() -> None:
     assert sumar_numeros_impares([1, 2, 3, 4, 5, 6]) == 9
+    # no hay pares
+    assert sumar_numeros_impares([2, 4, 6]) == 0
+    # no hay numeros
+    assert sumar_numeros_impares([]) == 0
